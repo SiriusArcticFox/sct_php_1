e67451c89e1cc8c0a6ac0bd8926f5c229633dad3
MaSirius <sirius_4752@outlook.com>
2021-06-14 23:17:42 +0400

    Added files: /index.php, /php/1.php, /php/2.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="ru">
+    <head>
+        <title>Урок №3</title>
+    </head>
+    <body>
+        <h1>
+            <?php echo 'Приветствую'; ?>
+        </h1>
+        <a href="/">Ссылка на самого себя</a><br/>
+        <a href="/php/1.php">Ссылка на файл 1.php</a><br/>
+        <a href="/php/2.php">Ссылка на файл 2.php</a>
+    </body>
+</html>
---
+++ php/1.php
@@ -0,0 +1,14 @@
+<html lang="ru">
+    <head>
+        <title>Урок №3</title>
+    </head>
+    <body>
+        <h2>
+            <?php echo 'Приветствую в файле 1.php'; ?>
+        </h2>
+        <a href="/php/1.php">Ссылка на самого себя</a><br/>
+        <a href="/php/2.php">Ссылка на файл 2.php</a><br/>
+        <a href="/">Ссылка на файл index.php</a>
+    </body>
+</html>
+
