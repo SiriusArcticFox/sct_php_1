45c633b45b118e04b26e5f9b300cf85dc194963a
MaSirius <sirius_4752@outlook.com>
2021-06-26 12:05:50 +0400

    Added echo-command and tag <img>, changed links.

--- php/1.php
+++ php/1.php
@@ -6,9 +6,9 @@
         <h2>
             <?php echo 'Приветствую в файле 1.php'; ?>
         </h2>
-        <a href="/php/1.php">Ссылка на самого себя</a><br/>
-        <a href="/php/2.php">Ссылка на файл 2.php</a><br/>
-        <a href="/">Ссылка на файл index.php</a>
+        <a href="<?='/php/1.php'?>">Ссылка на самого себя</a><br/>
+        <a href="<?='/php/2.php'?>">Ссылка на файл 2.php</a><br/>
+        <a href="<?='/'?>">Ссылка на файл index.php</a>
     </body>
 </html>
 
