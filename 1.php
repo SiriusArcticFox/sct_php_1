8dd0b6a48a2473340af168e0c09a9d0ebc9978be
MaSirius <sirius_4752@outlook.com>
2021-09-06 22:19:20 +0400

    12 lesson

--- 1.php
+++ 1.php
@@ -1,28 +1,44 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>Урок №11</title>
+    <title>Урок №12</title>
 </head>
 <body>
 <?php
-function manInfo ($name, $age, $adr) {
-    $age = abs($age);
-    $rest = 0;
+    /**
+     * Функция выводит возраст, адрес и имя человека
+     *
+     * @param string $name Имя
+     * @param string $adr Адрес (необязательное значение)
+     * @param int $age Возраст (необязательное значение)
+     * @return string
+     */
 
-    if ($age > 0) $rest = $age;
-    if ($age > 10) $rest = $age % 10;
-    if ($age > 100) $rest = $age % 100;
+    function manInfo ($name, $age = null, $adr = null) {
+        $result = "Имя: $name. ";
+        if (is_int($age)) {
+            $age = abs($age);
+            $rest = 0;
 
-    echo "Имя: $name<br/>";
-    echo "Возраст: $age";
+            if ($age > 0) $rest = $age;
+            if ($age > 10) $rest = $age % 10;
+            if ($age > 100) $rest = $age % 100;
 
-    if ($rest == 1) echo ' год';
-    if ($rest > 1 and $rest < 5 and ($age < 5 or $age > 20)) echo ' года';
-    if ($rest > 4 or $rest == 0 or ($age > 4 and $age < 21)) echo ' лет';
-    echo "<br/>";
-    echo "Адрес: $adr<br/>";
-}
+            $result .= "Возраст: $age ";
 
-manInfo('Василий', 104, 'ул. Иванова, 20/2');
+            if ($rest == 1) $result .= ' год. ';
+            if ($rest > 1 and $rest < 5 and ($age < 5 or $age > 20)) $result .= ' года. ';
+            if ($rest > 4 or $rest == 0 or ($age > 4 and $age < 21)) $result .= ' лет. ';
+        }
+        if (is_string($adr) && $adr != '') {
+            $result .= "Адрес: $adr.";
+        }
+        return $result . "<br/>";
+    }
+
+
+    echo manInfo('Василий', 14, 'ул. Иванова, 20/2');
+    echo manInfo('Николай', null, 'ул. Островского, 4/4');
+    echo manInfo('Александр');
 ?>
 </body>
--- 2.php
+++ 2.php
@@ -1,18 +1,31 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>Урок №11</title>
+    <title>Урок №12</title>
 </head>
 <body>
 <?php
-function getResult ($first, $second, $name) {
-    if ($name == 'sum') echo ($first + $second);
-    if ($name == 'subtr') echo ($first - $second);
-    if ($name == 'mult') echo ($first * $second);
-    if ($name == 'div') echo  ($first / $second);
-    echo '<br/>';
-}
+    /**
+     * Возвращает наибольшее значение
+     * @param integer $a
+     * @param integer $b
+     * @param integer $c (необязательное значение)
+     * @return int
+     */
+    function getResult ($a, $b, $c = null) {
+        echo "Переданные значения: $a, $b, $c.<br/>";
+        $result = $a;
+        if ($b > $a) $result = $b;
+        if (!is_null($c)) {
+            $c = (int)$c;
+            echo "c = (int)$c<br/>";
+            if ($c > $result) $result = $c;
+        }
+        return $result;
+    }
 
-getResult(47,52, 'subtr');
+    echo 'Наибольшее значение ' . getResult(47,52, 'su2btr21') . '<br/><br/>';
+    echo 'Наибольшее значение ' . getResult(7,-52, 20) . '<br/><br/>';
+    echo 'Наибольшее значение ' . getResult(7,-52) . '<br/><br/>';
 ?>
 </body>
