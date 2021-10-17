81ecdaf29541178ea7e6c1d5920796fd1e8b69fe
MaSirius <sirius_4752@outlook.com>
2021-10-17 22:29:19 +0300

    19 lesson

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Урок №19</title>
+</head>
+<body>
+<a href="?page=1">Калькулятор</a><br/>
+<a href="?page=2">2-ая страница</a><br/>
+<a href="?page=3">3-я страница</a><br/>
+<hr/>
+<?php
+    switch ($_GET['page']) {
+        case '2':
+            include 'page/2.php';
+            break;
+        case '3';
+            include 'page/3.php';
+            break;
+        default:
+            include 'page/1.php';
+    }
+?>
+</body>
+</html>
--- page/3.php
+++ page/3.php
@@ -1,63 +1,30 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>Урок №18</title>
-    <style>
-        a { display: block; }
-
-    </style>
+    <title>Урок №19</title>
 </head>
 <body>
-<a href="?num=1">Первое четверостишье</a>
-<a href="?num=2">Второе четверостишье</a>
-<a href="?num=3">Третье четверостишье</a>
-<a href="?num=4">Четвёртое четверостишье</a>
-<hr/>
-<?php
-$first = "
 Сегодня синели лужи<br/>
 И легкий шептал ветерок.<br/>
 Знай, никому не нужен<br/>
 Неба зеленый песок.<br/><br/>
-";
-$second = "
+
 Жили и были мы в яви,<br/>
 Всюду везде одни.<br/>
 Ты, как весну по дубраве,<br/>
 Пьешь свои белые дни.<br/><br/>
-";
-$third = "
+
 Любишь ты, любишь, знаю,<br/>
 Нежные души ласкать,<br/>
 Но не допустит нас к раю<br/>
 Наша земная печать.<br/><br/>
-";
 
-$fourth = "
 Вечная даль перед нами,<br/>
 Путь наш задумчив и прост.<br/>
 Даст нам приют за холмами<br/>
-Грязью покрытый погост.
-";
+Грязью покрытый погост.<br/>
 
-switch ($_GET['num']) {
-    case 1:
-        echo $first;
-        break;
-    case 2:
-        echo $second;
-        break;
-    case 3:
-        echo $third;
-        break;
-    case 4:
-        echo $fourth;
-        break;
-    default:
-        echo $first . $second . $third . $fourth;
-}
+<img src="img/3.jpg" alt="">
 
-
-?>
 </body>
 </html>
